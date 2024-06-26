eaa820634fb68d484669610762a39db80a83d7d3
Denys Sizomin <denys.sizomin@gmail.com>
2024-06-26 18:04:32 +0200

    Coc improvements.

--- nvim/lua/plugins/coc.lua
+++ nvim/lua/plugins/coc.lua
@@ -1,14 +1,21 @@
 return {
   'neoclide/coc.nvim',
   branch = 'release',
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
   keys = {
-    { "<leader>tv", "<cmd>CocCommand tsserver.chooseVersion<cr>", desc = "Choose tsserver version"},
+  { "<leader>tv", "<cmd>CocCommand tsserver.chooseVersion<cr>", desc = "Choose tsserver version"},
   },
   config = function()
     vim.g.coc_global_extensions={'coc-tsserver', 'coc-json', 'coc-eslint', 'coc-snippets'}
     vim.g.coc_user_config = {
-      ['eslint.autoFixOnSave'] = true
+      ['eslint.autoFixOnSave'] = true,
+      ['diagnostic.errorSign'] = '',
+      ['diagnostic.warningSign'] = '',
+      ['diagnostic.infoSign'] = '',
+      ['diagnostic.hintSign'] = '',
     }
     vim.cmd [[ source ~/coc.nvim/doc/coc-example-config.vim ]]
   end
-} 
+}
