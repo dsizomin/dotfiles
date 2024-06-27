93417264b8847ad02b52d0b3f773b61eefddaf4f
Denys Sizomin <denys.sizomin@gmail.com>
2024-06-27 10:51:53 +0200

    ALE linters 2.

--- nvim/lua/plugins/ale.lua
+++ nvim/lua/plugins/ale.lua
@@ -4,10 +4,13 @@ return {
     vim.g.ale_sign_column_always = 1
     vim.o.signcolumn = 'yes'
 
+    vim.g.ale_linters = {
+      typescript = {'eslint'},
+    }
+
     vim.g.ale_fixers = {
       ['*'] = {'remove_trailing_lines', 'trim_whitespace'},
-      javascript = {'prettier', 'eslint'},
-      typescript = {'prettier', 'eslint'},
+      typescript = {'eslint'},
     }
 
     vim.g.ale_fix_on_save = 1
