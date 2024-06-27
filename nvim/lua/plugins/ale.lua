07ee5772c2807698fdae9e1a44992adeecd222f2
Denys Sizomin <denys.sizomin@gmail.com>
2024-06-27 10:41:33 +0200

    ALE linters.

--- nvim/lua/plugins/ale.lua
+++ nvim/lua/plugins/ale.lua
@@ -4,6 +4,14 @@ return {
     vim.g.ale_sign_column_always = 1
     vim.o.signcolumn = 'yes'
 
+    vim.g.ale_fixers = {
+      ['*'] = {'remove_trailing_lines', 'trim_whitespace'},
+      javascript = {'prettier', 'eslint'},
+      typescript = {'prettier', 'eslint'},
+    }
+
+    vim.g.ale.fix_on_save = 1
+
     vim.cmd [[
       sign define DiagnosticSignError text= texthl=DiagnosticSignError
       sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn
