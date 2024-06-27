6f28b19d989f02e4b6ec4525f91affcbd53bfd8e
Denys Sizomin <denys.sizomin@gmail.com>
2024-06-27 10:54:45 +0200

    Revert ALE linters.

--- nvim/lua/plugins/ale.lua
+++ nvim/lua/plugins/ale.lua
@@ -4,17 +4,6 @@ return {
     vim.g.ale_sign_column_always = 1
     vim.o.signcolumn = 'yes'
 
-    vim.g.ale_linters = {
-      typescript = {'eslint'},
-    }
-
-    vim.g.ale_fixers = {
-      ['*'] = {'remove_trailing_lines', 'trim_whitespace'},
-      typescript = {'eslint'},
-    }
-
-    vim.g.ale_fix_on_save = 1
-
     vim.cmd [[
       sign define DiagnosticSignError text= texthl=DiagnosticSignError
       sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn
--- nvim/lua/plugins/coc.lua
+++ nvim/lua/plugins/coc.lua
@@ -8,9 +8,9 @@ return {
   { "<leader>tv", "<cmd>CocCommand tsserver.chooseVersion<cr>", desc = "Choose tsserver version"},
   },
   config = function()
-    vim.g.coc_global_extensions={'coc-tsserver', 'coc-json', 'coc-snippets'}
+    vim.g.coc_global_extensions={'coc-tsserver', 'coc-json', 'coc-eslint', 'coc-snippets'}
     vim.g.coc_user_config = {
-      -- ['eslint.autoFixOnSave'] = true,
+      ['eslint.autoFixOnSave'] = true,
       ['diagnostic.displayByAle'] = true,
     }
     vim.cmd [[ source ~/coc.nvim/doc/coc-example-config.vim ]]
