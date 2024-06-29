cc92f1a75603d054f2433211aa09f4a630b4f9a2
Denys Sizomin <denys.sizomin@gmail.com>
2024-06-29 03:12:16 +0200

    Final fix

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -31,31 +31,4 @@ return {
       end
     end
   }
-  -- "mfussenegger/nvim-lint",
-  -- config = function()
-  --   local lint = require("lint")
-  --   lint.linters_by_ft = {
-  --     javascript = {
-  --       "eslint_d"
-  --     },
-  --     typescript = {
-  --       "eslint_d"
-  --     },
-  --     javascriptreact = {
-  --       "eslint_d"
-  --     },
-  --     typescriptreact = {
-  --       "eslint_d"
-  --     },
-  --     lua = {
-  --       "luacheck"
-  --     }
-  --   }
-  --
-  --   vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
-  --     callback = function()
-  --       lint.try_lint()
-  --   end,
-  -- })
-  -- end
 }
