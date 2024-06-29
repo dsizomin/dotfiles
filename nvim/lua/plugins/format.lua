8e0f5eeb5b5802f176cf043328e0f786f74145db
Denys Sizomin <denys.sizomin@gmail.com>
2024-06-29 23:55:46 +0200

    Formatting.

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -1,26 +1,35 @@
 return {
-  -- 'stevearc/conform.nvim',
-  -- opts = {},
-  -- config = function()
-  --   local conform = require('conform')
-  --   conform.setup({
-  --     formatters_by_ft = {
-  --       -- lua = { "stylua" },
-  --       -- Conform will run multiple formatters sequentially
-  --       -- python = { "isort", "black" },
-  --       javascript = { "eslint_d" },
-  --       typescript = { "eslint_d" },
-  --       javascriptreact = { "eslint_d" },
-  --       typescriptreact = { "eslint_d" },
-  --       lua = { "luaformatter" }
-  --     },
-  --   })
-  --
-  --   vim.api.nvim_create_autocmd("BufWritePre", {
-  --    pattern = "*",
-  --    callback = function(args)
-  --       conform.format({ bufnr = args.buf })
-  --    end,
-  --   })
-  -- end,
+	"williamboman/mason.nvim",
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+		config = function()
+			local conform = require("conform")
+			conform.setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					-- Conform will run multiple formatters sequentially
+					-- python = { "isort", "black" },
+					javascript = { "prettierd" },
+					typescript = { "prettierd" },
+					javascriptreact = { "prettierd" },
+					typescriptreact = { "prettierd" },
+					-- lua = { "luaformatter" }
+				},
+			})
+
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function(args)
+					conform.format({ bufnr = args.buf })
+				end,
+			})
+		end,
+	},
+	{
+		"zapling/mason-conform.nvim",
+		config = function()
+			require("mason-conform").setup({})
+		end,
+	},
 }
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,14 +1,12 @@
 return {
   {
     "williamboman/mason.nvim",
-    lazy = true,
     config = function()
       require("mason").setup()
     end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    lazy = true,
     dependencies = {
       "williamboman/mason.nvim"
     },
