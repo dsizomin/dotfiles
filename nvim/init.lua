3dd6e979da314b1478f430a9d2652eadeba3470a
Denys Sizomin <denys.sizomin@gmail.com>
2024-07-07 22:20:41 +0200

    poc

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,41 +1,39 @@
-vim.cmd([[
-  set nocompatible            " disable compatibility to old-time vi
-  set showmatch               " show matching
-  set ignorecase              " case insensitive
-  set mouse=v                 " middle-click paste with
-  set hlsearch                " highlight search
-  set incsearch               " incremental search
-  set tabstop=2               " number of columns occupied by a tab
-  set softtabstop=2           " see multiple spaces as tabstops so <BS> does the right thing
-  set expandtab               " converts tabs to white space
-  set shiftwidth=2            " width for autoindents
-  set autoindent              " indent a new line the same amount as the line just typed
-  set number                  " add line numbers
-  set wildmode=longest,list   " get bash-like tab completions
-  filetype plugin indent on   " allow auto-indenting depending on file type
-  syntax on                   " syntax highlighting
-  set mouse=a                 " enable mouse click
-  set clipboard=unnamedplus   " using system clipboard
-  filetype plugin on
-  set ttyfast                 " Speed up scrolling in Vim
-]])
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.mapleader = " "
 
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
+if not vim.loop.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
 
-vim.o.signcolumn = "yes"
+vim.opt.rtp:prepend(lazypath)
 
-vim.diagnostic.config({
-	severity_sort = true,
-})
+local lazy_config = require "configs.lazy"
 
-vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+    config = function()
+      require "options"
+    end,
+  },
 
-require("config.lazy")
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
