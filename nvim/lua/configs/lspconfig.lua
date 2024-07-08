ca5106a3a81d8cb0fe5014b4d0e6af1088453683
Denys Sizomin <denys.sizomin@gmail.com>
2024-07-08 15:13:27 +0200

    Saga.

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,10 +1,14 @@
--- EXAMPLE 
+-- EXAMPLE
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = {}
+
+local cwd = vim.fn.getcwd()
+local lsputils = require "lspconfig.util"
+local root_dir = lsputils.root_pattern ".yarn"(cwd)
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -16,8 +20,33 @@ for _, lsp in ipairs(servers) do
 end
 
 -- typescript
-lspconfig.tsserver.setup {
+lspconfig.tsserver.setup(cwd:match "web%-code" and {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
-}
+  init_options = {
+    maxTsServerMemory = 8192,
+    tsserver = {
+      path = root_dir .. "/.yarn/sdks/typescript/lib/tsserver.js",
+    },
+  },
+} or {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+})
+
+-- eslint
+lspconfig.eslint.setup(cwd:match "web%-code" and {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  workingDirectory = { mode = "auto" },
+  settings = {
+    nodePath = root_dir .. "/.yarn/sdks",
+  },
+} or {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+})
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
@@ -10,6 +10,9 @@ return {
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason-lspconfig",
+    },
     config = function()
       require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
@@ -17,22 +20,43 @@ return {
   },
 
   {
-  	"williamboman/mason.nvim",
-  	opts = {
-  		ensure_installed = {
-  			"lua-language-server", "stylua",
-  			"html-lsp", "css-lsp" , "prettier"
-  		},
-  	},
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+    config = function()
+      require("mason-lspconfig").setup {
+        automatic_installation = {
+          exclude = { "eslint" },
+        },
+        ensure_installed = {
+          "eslint@4.8.0",
+        },
+      }
+    end,
   },
 
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css"
-  		},
-  	},
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+      },
+    },
+  },
+  {
+    "nvimdev/lspsaga.nvim",
+    event = 'LspAttach',
+    config = function()
+      require("lspsaga").setup {}
+    end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter", -- optional
+      "nvim-tree/nvim-web-devicons", -- optional
+    },
   },
 }
