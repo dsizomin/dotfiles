d3af8772f1531c055af6213cad983d81f9487dca
Denys Sizomin <denys.sizomin@gmail.com>
2024-07-09 01:03:02 +0200

    More good stuff.

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,4 +1,5 @@
 return {
+  { "lbrayner/vim-rzip", lazy = false },
   {
     "stevearc/conform.nvim",
     event = "BufWritePre", -- uncomment for format on save
@@ -12,6 +13,7 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
       "williamboman/mason-lspconfig",
+      "pmizio/typescript-tools.nvim",
     },
     config = function()
       require("nvchad.configs.lspconfig").defaults()
@@ -50,13 +52,60 @@ return {
   },
   {
     "nvimdev/lspsaga.nvim",
-    event = 'LspAttach',
+    -- event = "LspAttach",
     config = function()
       require("lspsaga").setup {}
     end,
+    keys = {},
     dependencies = {
       "nvim-treesitter/nvim-treesitter", -- optional
       "nvim-tree/nvim-web-devicons", -- optional
     },
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = {
+      enable = false,
+    },
+    panel = {
+      enable = false,
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+          require("copilot_cmp").setup()
+        end,
+      },
+    },
+    opts = {
+      sources = {
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "copilot", group_index = 2 },
+        { name = "luasnip", group_index = 2 },
+        { name = "buffer", group_index = 2 },
+        { name = "nvim_lua", group_index = 2 },
+        { name = "path", group_index = 2 },
+      },
+    },
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>ft", "<cmd>Telescope file_browser<CR>", { noremap = true, silent = true } },
+    },
+    config = function()
+      require("telescope").load_extension "file_browser"
+    end,
   },
 }
