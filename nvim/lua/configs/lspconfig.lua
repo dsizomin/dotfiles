3dff8ad466e5f6895e8dd5b0399c5d82b73c04b3
Denys Sizomin <denys.sizomin@gmail.com>
2024-07-18 13:48:24 +0200

    lazygit

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -38,7 +38,7 @@ require("typescript-tools").setup {
   on_attach = on_attach,
   settings = {
     -- spawn additional tsserver instance to calculate diagnostics on it
-    separate_diagnostic_server = true,
+    separate_diagnostic_server = false,
     -- "change"|"insert_leave" determine when the client asks the server about diagnostic
     publish_diagnostic_on = "change",
     -- array of strings("fix_all"|"add_missing_imports"|"remove_unused"|
