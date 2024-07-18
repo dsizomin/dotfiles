edf84cb015ada2ff978e2c407eb4b97db7b8cd8f
Denys Sizomin <denys.sizomin@gmail.com>
2024-07-18 15:26:06 +0200

    chadrc updates

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,16 +1,24 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "chadracula",
+  theme = "chadracula",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  transparency = true,
+
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+  statusline = {
+    theme = "minimal",
+  },
+  nvdash = {
+    load_on_startup = true,
+  }
 }
 
 return M
