c2dd59432dbc5ef78b34a56bed15ee2b62bc5327
Denys Sizomin <denys.sizomin@gmail.com>
2024-07-23 18:54:25 +0200

    markdown

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -235,4 +235,14 @@ return {
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
   },
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    main = "render-markdown",
+    opts = {},
+    ft = "markdown",
+    -- name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+  },
 }
