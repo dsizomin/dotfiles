0b1c53351e4da01efb65612751ff794891fdea0d
Denys Sizomin <denys.sizomin@gmail.com>
2024-08-17 22:23:11 +0200

    Gitsigns

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -173,10 +173,22 @@ return {
     opts = function()
       return {
         signs = {
-          delete = { text = "󰍵" },
-          changedelete = { text = "󱕖" },
+          add = { text = "┃" },
+          change = { text = "┃" },
+          delete = { text = "━" },
+          topdelete = { text = "‾" },
+          changedelete = { text = "" },
+          untracked = { text = "┆" },
         },
-
+        signs_staged = {
+          add = { text = "┃" },
+          change = { text = "┃" },
+          delete = { text = "━" },
+          topdelete = { text = "‾" },
+          changedelete = { text = "" },
+          untracked = { text = "┆" },
+        },
+        current_line_blame = true,
         on_attach = function(bufnr)
           local gs = package.loaded.gitsigns
 
