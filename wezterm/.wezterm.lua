d46d63ed3e701802a5cdd53ab641485e97672c15
Denys Sizomin <denys.sizomin@gmail.com>
2024-10-23 15:30:35 +0200

    wezterm theme
    This reverts commit 5c77f44e6d1a592da4eea14c2276853fff6b0215.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,17 +7,18 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
--- config.color_scheme = 'Dracula (Gogh)'
+config.colors = require("cyberdream")
 config.colors = {
 	background = "black",
 }
 
 config.window_background_opacity = 0.5
-config.macos_window_background_blur = 20
+config.macos_window_background_blur = 30
 
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 16
 
 config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
 -- and finally, return the configuration to wezterm
 return config
