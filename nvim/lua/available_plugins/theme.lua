5739a33ed05274378082030e2600dbaa80d2d9ef
Denys Sizomin <denys.sizomin@gmail.com>
2025-11-26 23:58:42 +0100

    hl

--- nvim/lua/available_plugins/theme.lua
+++ nvim/lua/available_plugins/theme.lua
@@ -3,6 +3,14 @@ return {
     "scottmckendry/cyberdream.nvim",
     lazy = false,
     priority = 1000,
+    opts = {
+      highlights = function()
+        local colors = require("cyberdream.colors").default
+        return {
+          ["@lsp.type.property"] = { fg = colors.pink },
+        }
+      end,
+    },
   },
   {
     "LazyVim/LazyVim",
