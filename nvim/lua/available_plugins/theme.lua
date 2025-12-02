4e4358b9ad31dfc86287d5d4683fecf073bb5266
Denys Sizomin <denys.sizomin@gmail.com>
2025-12-02 13:08:14 +0100

    updates

--- nvim/lua/available_plugins/theme.lua
+++ nvim/lua/available_plugins/theme.lua
@@ -4,10 +4,14 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
+      transparent = true,
+      hide_fillchars = false,
+      borderless_pickers = false,
       highlights = function()
         local colors = require("cyberdream.colors").default
         return {
           ["@lsp.type.property"] = { fg = colors.pink },
+          NonText = { fg = colors.grey },
         }
       end,
     },
